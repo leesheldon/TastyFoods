bc9050901ddc334248a68c12a10921970974bb7a
Han Co <lhanco@gmail.com>
2018-10-23 09:59:50 +0700

    Add project files.

---
+++ TastyFood/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TastyFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
